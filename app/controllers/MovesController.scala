8d536c82d6691b0cdb516b9098da4d82d6ca71f1
Renzo Ramos <renzoramosivan@gmail.com>
2026-05-12 12:13:52 +0200

    fix(controllers): ajustar pageSize a 12 en MovesController para mostrar exactamente 3 filas de 4 columnas

--- app/controllers/MovesController.scala
+++ app/controllers/MovesController.scala
@@ -18,7 +18,7 @@ class MovesController @Inject() (
 )(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   private val baseUrl  = config.get[String]("pokeapi.base-url")
-  private val pageSize = 20
+  private val pageSize = 12
 
   def index(page: Int): Action[AnyContent] = Action.async { implicit request =>
     val offset = (page - 1) * pageSize
